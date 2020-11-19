353f4fcb13954e81181c2e055a24374a185ed97a
tarzan114 <tanzirh10@gmail.com>
2020-11-18 23:51:22 -0500

    Database coneection established

--- config/Database.php
+++ config/Database.php
@@ -8,27 +8,30 @@ private $socket;
 private $user;
 private $password;//could be protected/static
 private  $dbname;
-
+private $con;
 
 
 public function connect(){
 // Constructor to connect to our DB
 
-$this->$host="localhost";
-$this->$port=3306;
-$this->$socket="";
-$this->$user="root";
-$this->$password="admin";
-$this->$dbname="librarydatabase";
+$this->host="localhost";
+$this->port=3306;
+$this->socket="";
+$this->user="root";
+$this->password="tarzan114";
+$this->dbname="librarydatabase";
+$this->charset="utf8mb4";
 
 
-$con = new mysqli($host, $user, $password, $dbname, $port, $socket)
-	or die ('Could not connect to the database server' . mysqli_connect_error());
+try {
+	$pdo = new PDO("mysql:host=$this->host;dbname=$this->dbname", $this->user, $this->password);
+	$pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+	echo "<h1>connected</h1>";
+	return $pdo;
+} catch (\Exception $e) {
+	echo "Connection failed: ".$e->getMessage();
+}
+
 }
 }
-//$con->close();
-// printf("Library Database");
-
-
 ?>
-<h1>librarydatabase</h1>
---
+++ config/index.php
@@ -0,0 +1,14 @@
+<?php
+include_once 'Database.php';
+?>
+<!DOCTYPE html>
+<html>
+<head>
+</head>
+<body>
+	<?php
+    $object=new LibraryDatabase;
+    $object->connect();
+  ?>
+</body>
+</html>
