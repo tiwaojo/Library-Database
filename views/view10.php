ad7467ff8e27feaa93b62d2bdbeb7daa2816c123
100707721 <62113312+100707721@users.noreply.github.com>
2020-11-22 17:21:17 -0500

    Create view10.php and updated view7

--- views/view10.php
+++ views/view10.php
@@ -5,8 +5,6 @@ header("Access-Control-Allow-Orgin: *");
 header("Content-Type: application/json");
 
 
-
-include('../models/Employees.php');
 include('../config/Database.php');
 
 
@@ -15,40 +13,38 @@ $database = new LibraryDatabase();
 $db = $database->connect();
 
 // $employee = new Employee($db);
-$query = "SELECT * FROM `employees at library 1`";
+$query = "SELECT * FROM `view 10`";
        
        $stmt = $db->prepare($query);
         // $stmt->bindParam(1, $this->id);
         $stmt->execute();
 
+//blog post query
+// $res = $employee->view1();
+
 //get row count
 $num = $stmt->rowCount();
 if ($num > 0) {
-    $employee_arr = array();
-    $employee_arr['data'] = array();
+    $view_arr = array();
+    $view_arr['data'] = array();
 
     while ($row = $stmt->fetch(PDO::FETCH_ASSOC)) {
         extract($row);
 
 
-        $employee_item = array(
-            'Employees_Id' => $Employees_Id,
-            'E_FName' => $E_FName,
-            'E_LName' => $E_LName,
-            // 'E_BDate' => $E_BDate,
-            'E_Email' => $E_Email,
-            // 'E_Address' => $E_Address,
-            // 'E_Sex' => $E_Sex,
-            'E_Username' => $E_Username,
-            'E_Password' => $E_Password,
+        $view_item = array(
+            'ISBN' => $ISBN,
+            'Book_Title' => $Book_Title,
+            'Publisher_Id' => $Publisher_Id,            
+            'Genre_Id' => $Genre_Id, 
         );
 //  echo "<tr><td>".$Employees_Id."</td><td>".$E_FName."</td><td>".$E_LName."</td><td>".$E_Email."</td><td>".$E_Username."</td><td>".$E_Password."</td></tr>";
 // echo $row["employees_Id"];
         //push data
-       array_push($employee_arr['data'], $employee_item);
+       array_push($view_arr['data'], $view_item);
     }
     //turn to json & output
-    echo json_encode($employee_arr);
+    echo json_encode($view_arr);
 } else {
     echo json_encode(
         array("message" => 'No posts Found')
