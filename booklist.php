b8b9280f5ef2109d5ba2a4fbef1fef9bfe8107b7
aadi1998 <aadiraj09@yahoo.com>
2020-11-25 10:35:46 -0500

    ...

--- booklist.php
+++ booklist.php
@@ -23,11 +23,16 @@
                     <div class= "views">
                         <button class="dropbtn">Views</button>
                         <div class="dropdown-views">
-                            <a href="views/view1.php"> View 1</a>
+                        <a href="views/view1.php"> View 1</a>
                             <a href="views/view2.php"> View 2</a>
                             <a href="views/view3.php"> View 3</a>
                             <a href="views/view4.php"> View 4</a>
                             <a href="views/view5.php"> View 5</a>
+                            <a href="views/view6.php"> View 6</a>
+                            <a href="views/view7.php"> View 7</a>
+                            <a href="views/view8.php"> View 8</a>
+                            <a href="views/view9.php"> View 9</a>
+                            <a href="views/view10.php">View 10</a>
                         </div>
                     </div>
                 </nav>
--- librarylist.php
+++ librarylist.php
@@ -5,7 +5,7 @@
 
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Book List</title>
+    <title>Visit Us!</title>
     <link rel="stylesheet" href="CSS/navstyle.css">
 </head>
 
@@ -23,11 +23,17 @@
                     <div class= "views">
                         <button class="dropbtn">Views</button>
                         <div class="dropdown-views">
-                            <a href="views/view1.php"> View 1</a>
+                        <a href="view1.php"> View 1</a>
+                        <a href="views/view1.php"> View 1</a>
                             <a href="views/view2.php"> View 2</a>
                             <a href="views/view3.php"> View 3</a>
                             <a href="views/view4.php"> View 4</a>
                             <a href="views/view5.php"> View 5</a>
+                            <a href="views/view6.php"> View 6</a>
+                            <a href="views/view7.php"> View 7</a>
+                            <a href="views/view8.php"> View 8</a>
+                            <a href="views/view9.php"> View 9</a>
+                            <a href="views/view10.php">View 10</a>
                         </div>
                     </div>
                 </nav>
@@ -37,6 +43,7 @@
 <div class="Title">
 <h2>Display all Libraries</h2>
 </div>
+    <div class= "content-area">
     <table>
     <tr>
             <th>Id</th>
@@ -45,4 +52,5 @@
             <?php include 'LibDatabase/Library/readLib.php'; ?>
         </tr>
   </table>
+  </div>
   </body>
