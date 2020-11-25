4fa82ae6ffa5dad0b87fc74c876daf447615e8e3
100707721 <62113312+100707721@users.noreply.github.com>
2020-11-25 16:42:19 -0500

    Update register.php

--- register.php
+++ register.php
@@ -48,9 +48,9 @@ $M_Password=isset($_POST['password'])?$_POST['password']:die("Error: The data yo
   if (mysqli_num_rows($result) > 0) {
       echo "An account exists with these credentials";
   } else {
-      $insert_query="INSERT INTO members (M_FName, M_LName, M_BDate, M_Email, M_Address, M_Sex, M_Username, M_Password, M_PNumber)
+      $insert_query="INSERT INTO members (M_MemberM_FName, M_LName, M_BDate, M_Email, M_Address, M_Sex, M_Username, M_Password, M_PNumber)
       VALUES (
-        
+
       '".$M_FName."','"
       .$M_LName."','"
       .$M_BDate."','"
@@ -73,7 +73,7 @@ $M_Password=isset($_POST['password'])?$_POST['password']:die("Error: The data yo
     //   echo $M_Username;
     //   echo $M_Password;
 
-      
+
       $result=$mysqli->query($insert_query);
     //   echo $result;
       //takes us back to intended page after execution
