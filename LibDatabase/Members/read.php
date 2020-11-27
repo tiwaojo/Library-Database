af5a7c8452eba86017159fe4ad55a56de94bf69c
100707721 <62113312+100707721@users.noreply.github.com>
2020-11-26 19:00:14 -0500

    Update read.php

--- LibDatabase/Members/read.php
+++ LibDatabase/Members/read.php
@@ -1,74 +1,27 @@
 <?php
-//reads our data from the model and displays in JSON
+$servername = "localhost";
+$username = "root";
+$password = "qwer1234";
+$dbname = "librarydatabase";
 
-//headers
-//Thois is to allow others to gain access to our api. i.e. only specific tokens(people) can access our api.
-//In this case the '*' gives everyone access t access to
-header("Access-Control-Allow-Orgin: *");
-// header("Content-Type: application/json");
-
-<<<<<<< HEAD
-
-include_once('models/Members.php');
-include_once('config/Database.php');
-=======
-//header("Access-Control-Allow-Orgin: *");
-//header("Content-Type: application/json");
-
-
-
-
-
-include_once './models/Members.php';
-//include_once 'C:\wamp64\www\Library-Database/config/Database.php';
-
-class MemberRead extends Member
-{
->>>>>>> cb318a6c876e00b43c838bbbd9eb50e578b6e005
-
-
-//instantiate and connect to DB
-$database = new LibraryDatabase();
-$db = $database->connect();
-
-$member = new Members($db);
-
-//blog post query
-$res = $member->read();
-
-//get row count
-$num = $res->rowCount();
-if ($num > 0) {
-    $member_arr = array();
-    $member_arr['data'] = array();
-
-    while ($row = $res->fetch(PDO::FETCH_ASSOC)) {
-        extract($row);
-
-
-        $member_item = array(
-            'Members_Id' => $Members_Id,
-            'M_FName' => $M_FName,
-            'M_LName' => $M_LName,
-            'M_BDate' => $M_BDate,
-            'M_Email' => $M_Email,
-            'M_Address' => $M_Address,
-            'M_Sex' => $M_Sex,
-            'M_Username' => $M_Username,
-            'M_Password' => $M_Password,
-        );
- echo "<tr><td>".$Members_Id."</td><td>".$M_FName."</td><td>".$M_LName."</td></tr>";
-// echo $row["Members_Id"];
-        //push data
-       // array_push($member_arr['data'], $member_item);
-    }
-    //turn to json & output
-    // echo json_encode($member_arr);
-} else {
-    // echo json_encode(
-    //     array("message" => 'No posts Found')
-    // );
-    echo "<h1>There are 0 rows</h1>";
+// Create connection
+$conn = new mysqli($servername, $username, $password, $dbname);
+// Check connection
+if ($conn->connect_error) {
+  die("Connection failed: " . $conn->connect_error);
 }
 
-?>
+$sql = "SELECT Members_Id, M_FName, M_LName FROM members";
+$result = $conn->query($sql);
+
+if ($result->num_rows > 0) {
+  echo "<table><tr><th>ID</th><th>Name</th></tr>";
+  // output data of each row
+  while($row = $result->fetch_assoc()) {
+    echo "<tr><td>".$row["Members_Id"]."</td><td>".$row["M_FName"]." ".$row["M_LName"]."</td></tr>";
+  }
+  echo "</table>";
+} else {
+  echo "0 results";
+}
+$conn->close();
