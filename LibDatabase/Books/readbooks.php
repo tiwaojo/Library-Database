fea156da6879ebb32c60d506b411350bc7402410
100707721 <62113312+100707721@users.noreply.github.com>
2020-11-26 19:00:11 -0500

    Update readbooks.php

--- LibDatabase/Books/readbooks.php
+++ LibDatabase/Books/readbooks.php
@@ -1,114 +1,30 @@
 <?php
-//reads our data from the model and displays in JSON
+$servername = "localhost";
+$username = "root";
+$password = "qwer1234";
+$dbname = "librarydatabase";
 
-//headers
-//Thois is to allow others to gain access to our api. i.e. only specific tokens(people) can access our api.
-//In this case the '*' gives everyone access t access to
-header("Access-Control-Allow-Orgin: *");
-// header("Content-Type: application/json");
-
-
-
-include_once('../models/Books.php');
-include_once('../config/Database.php');
-/**
- *
- */
-class BooksRead extends Books
-{
-
-  function __construct($db)
-  {
-    // code...
-     parent::__construct($db);
-  }
-  public function getBookTable(){
-    $res=parent::read();
-    return $res;
-  }
-  public function getBookColumn($id){
-    $res=parent::readId($id);
-    return $res;
-  }
-  public function printBookColumnTable($response)
-  {
-    // code...
-    $num=$response->rowCount();
-    if ($num > 0) {
-        $Book_arr = array();
-        $Book_arr['data'] = array();
-
-        while ($row = $response->fetch(PDO::FETCH_ASSOC)) {
-            extract($row);
-            $Book_item = array(
-                'ISBN' => $ISBN,
-                'Book_Title'=>$Book_Title,
-                'Publisher_Id'=>$Publisher_Id,
-                'Genre_Id'=>$Genre_Id,
-                'Library_Id'=>$Library_Id,
-                'Author_Id'=>$Author_Id,
-            );
-
-    echo "<tr><td>".$ISBN."</td><td>".$Book_Title."</td><td>".$Publisher_Id."</td><td>".$Genre_Id."</td><td>".$Library_Id."</td><td>".$Author_Id."</td></tr>";
-    return $row;
-    // echo $row["librarys_Id"];
-            //push data
-            // array_push($library_arr['data'], $library_item);
-        }
-        //turn to json & output
-        // echo json_encode($library_arr);
-    } else {
-        // echo json_encode(
-        //     array("message" => 'No posts Found')
-        // );
-        echo "<h1>There are 0 rows</h1>";
-    }
-  }
-
+// Create connection
+$conn = new mysqli($servername, $username, $password, $dbname);
+// Check connection
+if ($conn->connect_error) {
+  die("Connection failed: " . $conn->connect_error);
 }
 
+$sql = "SELECT ISBN, Book_Title, Author_Id, Genre_Id FROM book";
+$result = $conn->query($sql);
+if (!$result) {
+    trigger_error('Invalid query: ' . $conn->error);
+}
 
-// //instantiate and connect to DB
-// $database = new LibraryDatabase();
-// $db = $database->connect();
-//
-// $book = new Books($db);
-//
-// //blog post query
-// $res = $book->read();
-//
-// //get row count
-// $num = $res->rowCount();
-// if ($num > 0) {
-//     $member_arr = array();
-//     $member_arr['data'] = array();
-//
-//     while ($row = $res->fetch(PDO::FETCH_ASSOC)) {
-//         extract($row);
-//
-//
-//         $member_item = array(
-//             'ISBN' => $ISBN,
-//             'Book_Title' => $Book_Title,
-//             'Publisher_Id' => $Publisher_Id,
-//             'Genre_Id' => $Genre_Id,
-//             'Library_Id' => $Library_Id,
-//             'Author_Id' => $Author_Id,
-//
-//         );
-//
-// echo "<tr><td>".$ISBN."</td><td>".$Book_Title."</td><td>".$Author_Id."</td><td>".$Genre_Id."</td></tr>";
-// // echo $row["Members_Id"];
-//         //push data
-//         // array_push($member_arr['data'], $member_item);
-//     }
-//     //turn to json & output
-//     // echo json_encode($member_arr);
-// } else {
-//     // echo json_encode(
-//     //     array("message" => 'No posts Found')
-//     // );
-//     echo "<h1>There are 0 rows</h1>";
-// }
-
-?>
+if ($result->num_rows > 0) {
+  echo "<table><tr><th>ISBN</th><th>Book Name</th><th>Author_Id</th><th>Genre Id</th></tr>";
+  // output data of each row
+  while($row = $result->fetch_assoc()) {
+    echo "<tr><td>".$row["ISBN"]."</td><td>".$row["Book_Title"]."</td><td> ".$row["Author_Id"]."</td><td> ".$row["Genre_Id"]."</td></tr>";
+  }
+  echo "</table>";
+} else {
+  echo "0 results";
+}
+$conn->close();
