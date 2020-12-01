f8f9d3ad2018b7400e833049fd5b3975d70f8713
100707721 <62113312+100707721@users.noreply.github.com>
2020-11-30 22:59:19 -0500

    Update member_search.php

--- views/member_search.php
+++ views/member_search.php
@@ -1,9 +1,9 @@
 
 <?php
- include('header.php');
+ include('header.php');//includes functions in header.php
 ?>
 
-<div class= "content-area"><h2>Display all books</h2>
+<div class= "content-area"><h2>Display all books</h2><!--table diplays all books inn database-->
     <table>
         <tr>
             <th>ISBN</th>
@@ -11,10 +11,10 @@
             <th>Author</th>
             <th>Genre</th>
         </tr>
-        
+
 
 <div class="content-area"> <h2>Search Book </h2>
-  <form class="" action="searchbook.php" method="post">
+  <form class="" action="searchbook.php" method="post"><!-- uses searchbook.php and post method-->
 <input type="text" placeholder="Search.." name="search">
 <button type="submit"><i class="fa fa-search"></i></button>
 </form>
