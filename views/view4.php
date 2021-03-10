46abf6e201ef01daf26c967f3103bb090de7505d
Aryandesai1 <aryandesai34@gmail.com>
2021-03-09 20:26:25 -0500

    Update view4.php

--- views/view4.php
+++ views/view4.php
@@ -8,62 +8,37 @@ include('../config/Database.php'); ?>
 
 <div class="content-area">
     <h2>View 4</h2>
-    <table>
 
-        <tr id="table-header">
-            <th>First Name</th>
-            <th>Last Name</th>
-            <th>Email</th>
-            <th>Address</th>
-            <th>Sex</th>
-            <th>Loan ID</th>
-            <th>Date Loaned</th>
-        </tr>
+
+
 
         <?php
         //instantiate and connect to DB
         $database = new LibraryDatabase();
-        $db = $database->connect();
-        $member = new Members($db);
+        $conn = $database->connectsqli();
 
         //sql query to insert data
         //Select Table
-        $query = "SELECT * FROM `view 4`";
+        $sql = "SELECT * FROM `view 4`";
 
-        $stmt = $db->prepare($query);
-
-        // Bind ID
-
-        // Execute query
-        $stmt->execute();
-
-
-        //get row count
-        $num = $stmt->rowCount();
-        if ($num > 0) {
-            $member_arr = array();
-            $member_arr['data'] = array();
-
-            while ($row = $stmt->fetch(PDO::FETCH_ASSOC)) {
-                extract($row);
-
-                // Select required rows to be displayed for view 4
-                $member_item = array(
-                    'M_FName' => $M_FName,
-                    'M_LName' => $M_LName,
-                    'M_Email' => $M_Email,
-                    'M_Address' => $M_Address,
-                    'M_Sex' => $M_Sex,
-                    'Loan_Id' => $Loan_Id,
-                    'Date_Loaned' => $Date_Loaned,
-                );
-                echo "<tr><td>" . $M_FName . "</td><td>" . $M_LName . "</td><td>" . $M_Email . "</td><td>" . $M_Address . "</td><td>" . $M_Sex . "</td><td>" . $Loan_Id . "</td><td>" . $Date_Loaned . "</td></tr>";
-            }
+        $result = $conn->query($sql);
+        if (!$result) {
+            trigger_error('Invalid query: ' . $conn->error);
+        }//if statement stating that its a connection error
+        //if table is there then it will echo a table for some book variables
+        if ($result->num_rows > 0) {
+          echo "<table><tr><th>First Name</th><th>Last Name</th><th>Email</th><th>Address</th><th>Sex</th><th>Loan ID</th><th>Date Loaned</th></tr>";
+          // output data of each row
+          while($row = $result->fetch_assoc()) {
+            echo "<tr><td>".$row["M_FName"]."</td><td>".$row["M_LName"]."</td><td> ".$row["M_Email"]."</td><td>".$row["M_Address"]."</td><td>".$row["M_Sex"]."</td><td>".$row["Loan_Id"]."</td><td>".$row["Date_Loaned"]."</td></tr>";
+          }
+          echo "</table>";
         } else {
-            echo "<h1>There are 0 rows</h1>";
+          echo "0 results";
         }
+        $conn->close();
 
         ?>
-    </table>
+
 </div>
 </body>
