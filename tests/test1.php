2c929bedb3c1af02a2d103a0e03ef586119e638d
tiwaojo <tiwaojo@gmail.com>
2021-04-07 21:30:11 -0400

    attempt @ unit & automated testing with phpunit

--- tests/test1.php
+++ tests/test1.php
@@ -1,42 +1,56 @@
 <?php
-//Run composer require to set up
 require 'vendor/autoload.php';
- 
-// selenium-server-standalone-#.jar (version 3.x)
-$host = 'http://localhost:4444/wd/hub';
-// selenium-server-standalone-#.jar (version 4.x)
-// $host = 'http://localhost:4444';
-
-//For testing on chrome
+// Run "composer require" to set up
+// Might need to run "composer dump-autoload -o"
 use PHPUnit\Framework\TestCase;
 use Facebook\WebDriver\Chrome\ChromeOptions;
 use Facebook\WebDriver\Chrome\ChromeDriver;
+use Facebook\WebDriver\Firefox\FirefoxDriver;
+use Facebook\WebDriver\Firefox\FirefoxProfile;
+use Facebook\WebDriver\Remote\DesiredCapabilities;
+use Facebook\WebDriver\Remote\RemoteWebDriver;
+use Facebook\WebDriver\WebDriverBy;
  
-protected $webDriver;
- 
-$capabilities = DesiredCapabilities::chrome();
-$this->webDriver = RemoteWebDriver::create($host, $capabilities);
-
- 
-class ClassNameTest extends TestCase
+class GoogleSearchChromeTest extends TestCase
 {
-    public function setUp(): void
-    {
-        /* Setup (or initialization) method goes here */
-    }
  
-    public function tearDown(): void
-    {
-        /* tearDown (or de-initialization) method goes here */
-    }
- 
-    /*
-     * test
+  protected $webDriver;
+  
+  public function build_chrome_capabilities(){
+    $capabilities = DesiredCapabilities::chrome();
+    return $capabilities;
+  }
+  
+  public function setUp(): void
+  {
+    $capabilities = $this->build_chrome_capabilities();
+    /* Download the Selenium Server 3.141.59 from 
+    https://selenium-release.storage.googleapis.com/3.141/selenium-server-standalone-3.141.59.jar
     */
-    public function test_scenario_1()
-    {
-        /* Test Method implementation goes here */
-        echo "Test scenario 1";
+    $this->webDriver = RemoteWebDriver::create('http://localhost:4444/wd/hub', $capabilities);
+  }
+ 
+  public function tearDown(): void
+  {
+    $this->webDriver->quit();
+  }
+  /*
+  * @test
+  */ 
+  public function test_searchTextOnGoogle()
+  {
+    $this->webDriver->get("https://www.google.com/ncr");
+    $this->webDriver->manage()->window()->maximize();
+    
+    sleep(5);
+    
+    $element = $this->webDriver->findElement(WebDriverBy::name("q"));
+    if($element) {
+      $element->sendKeys("LambdaTest");
+      $element->submit();
     }
+    
+    print $this->webDriver->getTitle();
+    $this->assertEquals('LambdaTest - Google Search', $this->webDriver->getTitle());
+  }
 }
-?>
